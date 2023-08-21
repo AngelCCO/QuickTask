c26038ad73aabf6a39daaf65b53db795d3335252
AngelCCO <116102695+AngelCCO@users.noreply.github.com>
2023-08-21 11:18:26 -0700

    Scripts

--- QuickTask - Angel Coronel/Assets/Scenes/Scripts/Movement.cs
+++ QuickTask - Angel Coronel/Assets/Scenes/Scripts/Movement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float speed = 8f;
+    private float jump = 8f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
     }
 }
