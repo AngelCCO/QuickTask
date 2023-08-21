d2f410f72435528b7cce69a9bf7dd708f676da27
AngelCCO <116102695+AngelCCO@users.noreply.github.com>
2023-08-21 11:43:15 -0700

    ok

--- QuickTask - Angel Coronel/Assets/Scenes/Scripts/Movement.cs
+++ QuickTask - Angel Coronel/Assets/Scenes/Scripts/Movement.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jump = 8f;
+    private Rigidbody2D rb2D;
 
+    private float moveSpeed;
+    private float jumpforce;
+    private float isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
 
-    // Update is called once per frame
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+        moveSpeed = 3f;
+        jumpforce = 60f;
+        isJumping = false;
+    }
+
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if(moveHorizontal > 0.1f  moveHorizontal < -0.1f)
+        {
+            rb2D.AddForce
+        }
     }
 }
